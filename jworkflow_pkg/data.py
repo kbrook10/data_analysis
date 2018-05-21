277ca496e08d8bd32c858987dae3c345f646af28
keith brooks <kbrook10@gmail.com>
2018-05-21 15:37:50 -0400

    Creating my first package

---
+++ jworkflow_pkg/data.py
@@ -0,0 +1,36 @@
+# Step 19: Import necessary libraries and clean up notebook flow
+import os
+from urllib.request import urlretrieve
+import pandas as pd
+
+# Step 1: Save URL for bike data
+
+BIKE_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+# Step 18: Only download data if it does not already exist
+def get_bdata(filename='fremont.csv', url=BIKE_URL, 
+    force_download=False):
+    """
+    Download and cache the bike data
+    
+    Parameters:
+    ----------
+    filename : string (optional)
+        location to same the data
+    url : string (optional)
+        web location of the data
+    force_download : bool (optional)
+        if True, force re-download of data
+
+    Returns:
+    --------
+    data : pandas.DataFrame
+        The bike data
+
+    """
+    if force_download or not os.path.exists(filename):
+        urlretrieve(URL,'fremont.csv')
+    bdata = pd.read_csv('fremont.csv', index_col='Date', parse_dates=True) 
+    bdata.columns = ['West', 'East']
+    bdata['Total'] = bdata['West'] + bdata['East']
+    return bdata
