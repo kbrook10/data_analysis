5aa02eb573e8eea667a9ebd8bd4d5e836dac8caa
keith brooks <kbrook10@gmail.com>
2018-05-22 13:25:20 -0400

    created unit tests and make file to simplify the process to call the test

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	python3 -m pytest jworkflow_pkg
---
+++ jworkflow_pkg/tests/test_data.py
@@ -0,0 +1,12 @@
+### Step 1: Unit Testing Import necessary libraries
+import pandas as pd
+from urllib.request import urlretrieve
+
+### Step 2: Import package to test                                            
+from jworkflow_pkg.data import get_bdata
+
+# Step 6: Place assertions into a function
+def test_bike_data():
+    data = get_bdata()
+    assert all(data.columns == ['West', 'East', 'Total'])
+    assert(isinstance(data.index, pd.DatetimeIndex))
